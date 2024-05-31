b998ea99e23bb0b7fcc3e767d7b321bba51d8d85
Redoan23 <redoan1080@gmail.com>
2024-05-31 22:56:31 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+
+
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
+
+
+export default router;
